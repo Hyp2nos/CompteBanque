1ca4e194e9508fd6795bc2da84e02a42b81ca339
Andriamiharisoa Mamy Fifaliana <85287596+Hyp2nos@users.noreply.github.com>
2021-11-11 16:44:31 +0300

    first commit

---
+++ src/test/java/com/compte/org/Finupp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compte.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finupp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
